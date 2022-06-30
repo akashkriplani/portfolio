e9bac50c0cdd9c9dc617065c7e1e446313515655
Akash Kriplani <aakash23kriplani@gmail.com>
2022-06-30 18:08:43 +0530

    Add side menu

--- src/app/components/navigation-menu/navigation-menu.component.ts
+++ src/app/components/navigation-menu/navigation-menu.component.ts
@@ -3,11 +3,12 @@ import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { NavMenuItems } from '../../constants';
 import { INavMenuItems } from '../../interfaces';
+import { ToggleNavMenuDirective } from '../../directives/toggle-nav-menu.directive';
 
 @Component({
   selector: 'app-navigation-menu',
   standalone: true,
-  imports: [CommonModule, RouterModule],
+  imports: [CommonModule, RouterModule, ToggleNavMenuDirective],
   templateUrl: './navigation-menu.component.html',
   styleUrls: ['./navigation-menu.component.scss'],
 })
