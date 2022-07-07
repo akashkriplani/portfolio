076657b62f53098cbf5d247585cd4be1dddc5b6d
Akash Kriplani <aakash23kriplani@gmail.com>
2022-07-07 19:29:07 +0530

    Add about me page

--- src/app/components/about/about.component.ts
+++ src/app/components/about/about.component.ts
@@ -1,18 +1,64 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  OnInit,
+  Renderer2,
+  ViewChild
+} from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import Blast from 'blast-vanilla';
 
 @Component({
   selector: 'app-about',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule],
   templateUrl: './about.component.html',
   styleUrls: ['./about.component.scss']
 })
-export class AboutComponent implements OnInit {
+export class AboutComponent implements OnInit, AfterViewInit {
+  @ViewChild('title') titlePieces!: ElementRef;
 
-  constructor() { }
+  constructor(private renderer: Renderer2) {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  ngAfterViewInit() {
+    new Blast('h1', {
+      returnGenerated: true,
+      delimiter: 'character',
+      tag: 'span',
+      search: false,
+      customClass: '',
+      aria: true,
+      debug: false,
+      name: 'blast'
+    });
+
+    let elements: HTMLCollection = this.titlePieces.nativeElement.children;
+    let timer = 0;
+    for (const element of Array.from(elements)) {
+      setTimeout(() => {
+        this.renderer.addClass(element, 'animated');
+        this.renderer.addClass(element, 'bounceIn');
+      }, timer);
+
+      timer += 100;
+
+      setTimeout(() => {
+        this.renderer.removeClass(element, 'animated');
+        this.renderer.removeClass(element, 'bounceIn');
+        this.renderer.setStyle(element, 'opacity', 1);
+        element.addEventListener('mouseenter', () => {
+          this.renderer.addClass(element, 'animated');
+          this.renderer.addClass(element, 'rubberBand');
+        });
+        element.addEventListener('mouseleave', () => {
+          this.renderer.removeClass(element, 'animated');
+          this.renderer.removeClass(element, 'rubberBand');
+        });
+      }, 1500);
+    }
   }
-
 }
