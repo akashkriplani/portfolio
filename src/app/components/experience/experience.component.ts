dac8916687b875b1944f19c6e054804ab4ba1205
Akash Kriplani <aakash23kriplani@gmail.com>
2024-07-20 03:50:54 +0530

    Add title to experience

--- src/app/components/experience/experience.component.ts
+++ src/app/components/experience/experience.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, Renderer2, ViewChild } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import Blast from 'blast-vanilla';
 import { TimelineDirective } from '../../directives/timeline.directive';
 import { EXPERIENCES } from '../../constants';
 import { IExperience } from '../../interfaces';
@@ -12,5 +13,46 @@ import { IExperience } from '../../interfaces';
   styleUrls: ['./experience.component.scss']
 })
 export class ExperienceComponent {
+  constructor(private renderer: Renderer2) {}
+
   experience: IExperience[] = EXPERIENCES;
+  @ViewChild('title') titlePieces!: ElementRef;
+
+  ngAfterViewInit() {
+    new Blast('h1', {
+      returnGenerated: true,
+      delimiter: 'character',
+      tag: 'span',
+      search: false,
+      customClass: '',
+      aria: true,
+      debug: false,
+      name: 'blast'
+    });
+
+    let elements: HTMLCollection = this.titlePieces.nativeElement.children;
+    let timer = 0;
+    for (const element of Array.from(elements)) {
+      setTimeout(() => {
+        this.renderer.addClass(element, 'animated');
+        this.renderer.addClass(element, 'bounceIn');
+      }, timer);
+
+      timer += 100;
+
+      setTimeout(() => {
+        this.renderer.removeClass(element, 'animated');
+        this.renderer.removeClass(element, 'bounceIn');
+        this.renderer.setStyle(element, 'opacity', 1);
+        element.addEventListener('mouseenter', () => {
+          this.renderer.addClass(element, 'animated');
+          this.renderer.addClass(element, 'rubberBand');
+        });
+        element.addEventListener('mouseleave', () => {
+          this.renderer.removeClass(element, 'animated');
+          this.renderer.removeClass(element, 'rubberBand');
+        });
+      }, 1500);
+    }
+  }
 }
